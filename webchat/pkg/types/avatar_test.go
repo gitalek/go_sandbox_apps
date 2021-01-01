52cbd86792158a96879e154ea31c5ac5fc23fcd0
Alexander <shalek.edu@mail.ru>
2021-01-01 23:13:33 +1000

    make lint

--- webchat/pkg/types/avatar_test.go
+++ webchat/pkg/types/avatar_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestAuthAvatar(t *testing.T) {
 	var authAvatar AuthAvatar
 	client := new(client)
-	url, err := authAvatar.GetAvatarURL(client)
+	_, err := authAvatar.GetAvatarURL(client)
 	if err != ErrNoAvatarURL {
 		t.Error("AuthAvatar.GetAvatarURL should return ErrNoAvatarURL when no value present")
 	}
@@ -13,7 +13,7 @@ func TestAuthAvatar(t *testing.T) {
 	// set a value
 	testUrl := "http://url-to-gravatar"
 	client.userData = map[string]interface{}{"avatar_url": testUrl}
-	url, err = authAvatar.GetAvatarURL(client)
+	url, err := authAvatar.GetAvatarURL(client)
 	if err != nil {
 		t.Error("AuthAvatar.GetAvatarURL should return no error when value present")
 	}
